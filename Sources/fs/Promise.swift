2dd83ecb80650aace9668569df9fbb8cf51de8d2
Helge Heß <devteam@zeezide.de>
2020-01-09 15:37:43 +0100

    Add some fs.promise based functions
    Going to make sense once we have async/await.

---
+++ Sources/fs/Promise.swift
@@ -0,0 +1,200 @@
+//
+//  Promise.swift
+//  Macro
+//
+//  Created by Helge Heß on 6/8/16.
+//  Copyright © 2016-2020 ZeeZide GmbH. All rights reserved.
+//
+
+import struct   NIO.ByteBuffer
+import struct   NIO.EventLoopPromise
+import class    NIO.EventLoopFuture
+import protocol NIO.EventLoop
+import class    NIO.NIOThreadPool
+import enum     NIO.ChannelError
+import class    MacroCore.MacroCore
+import enum     MacroCore.CharsetConversionError
+import enum     MacroCore.MacroError
+import struct   Foundation.Data
+import struct   Foundation.URL
+
+/**
+ * Node also provides a Promise based fs API:
+ * https://nodejs.org/dist/latest-v13.x/docs/api/fs.html#fs_fs_promises_api
+ *
+ * Those only really start to make sense w/ async/await, but since NIO is
+ * promise based anyways ...
+ */
+public enum promise {
+}
+
+public extension promise {
+  
+  // TODO: Complete me :-)
+  // Note: We dupe the imp, to avoid the extra promise overhead ¯\_(ツ)_/¯
+  
+  static func readFile(on eventLoop : EventLoop? = nil,
+                       _       path : String) -> EventLoopFuture<ByteBuffer>
+  {
+    let module    = MacroCore.shared.retain()
+    let eventLoop = module.fallbackEventLoop(eventLoop)
+    let promise   = eventLoop.makePromise(of: ByteBuffer.self)
+
+    FileSystemModule.threadPool.submit { shouldRun in
+      defer { module.release() }
+      
+      guard case shouldRun = NIOThreadPool.WorkItemState.active else {
+        return promise.fail(ChannelError.ioOnClosedChannel)
+      }
+      do {
+        let data = try Data(contentsOf: URL(fileURLWithPath: path))
+        var buf = MacroCore.shared.allocator.buffer(capacity: data.count)
+        buf.writeBytes(data)
+        promise.succeed(buf)
+      }
+      catch {
+        promise.fail(error)
+      }
+    }
+    
+    return promise.futureResult
+  }
+
+  static func readFile(on eventLoop : EventLoop? = nil,
+                       _       path : String,
+                       _   encoding : String.Encoding) -> EventLoopFuture<String>
+  {
+    let module    = MacroCore.shared.retain()
+    let eventLoop = module.fallbackEventLoop(eventLoop)
+    let promise   = eventLoop.makePromise(of: String.self)
+
+    FileSystemModule.threadPool.submit { shouldRun in
+      defer { module.release() }
+      
+      guard case shouldRun = NIOThreadPool.WorkItemState.active else {
+        return promise.fail(ChannelError.ioOnClosedChannel)
+      }
+
+      do {
+        let data = try Data(contentsOf: URL(fileURLWithPath: path))
+        guard let s = String(bytes: data, encoding: encoding) else {
+          promise.fail(CharsetConversionError
+                         .failedToConverData(encoding: encoding))
+          return
+        }
+        promise.succeed(s)
+      }
+      catch {
+        promise.fail(error)
+      }
+    }
+    
+    return promise.futureResult
+  }
+  
+  @inlinable
+  static func readFile(on eventLoop : EventLoop? = nil,
+                       _       path : String,
+                       _   encoding : String) -> EventLoopFuture<String>
+  {
+    return readFile(on: eventLoop, path, .encodingWithName(encoding))
+  }
+  
+  @inlinable
+  static func writeFile(on eventLoop : EventLoop? = nil,
+                        _ path: String, _ byteBuffer: ByteBuffer)
+              -> EventLoopFuture<Void>
+  {
+    var bb = byteBuffer
+    guard let data = bb.readData(length: bb.readableBytes) else {
+      let eventLoop = MacroCore.shared.fallbackEventLoop(eventLoop)
+      let promise   = eventLoop.makePromise(of: Void.self)
+      promise.fail(MacroError.failedToConvertByteBufferToData)
+      return promise.futureResult
+    }
+    return writeFile(on: eventLoop, path, data)
+  }
+
+  @inlinable
+  static func writeFile(on eventLoop : EventLoop? = nil,
+                        _       path : String,
+                        _     string : String,
+                        _   encoding : String.Encoding = .utf8)
+              -> EventLoopFuture<Void>
+  {
+    guard let data = string.data(using: encoding) else {
+      let eventLoop = MacroCore.shared.fallbackEventLoop(eventLoop)
+      let promise   = eventLoop.makePromise(of: Void.self)
+      promise.fail(CharsetConversionError
+                     .failedToConverData(encoding: encoding))
+      return promise.futureResult
+    }
+    return writeFile(path, data)
+  }
+  
+  @inlinable
+  static func writeFile(on eventLoop : EventLoop? = nil,
+                        _       path : String,
+                        _     string : String,
+                        _   encoding : String)
+              -> EventLoopFuture<Void>
+  {
+    return writeFile(on: eventLoop, path, string, .encodingWithName(encoding))
+  }
+  
+  @inlinable
+  static func writeFile(on eventLoop : EventLoop? = nil,
+                        _       path : String,
+                        _       data : Data) -> EventLoopFuture<Void>
+  {
+    let module    = MacroCore.shared.retain()
+    let eventLoop = module.fallbackEventLoop(eventLoop)
+    let promise   = eventLoop.makePromise(of: Void.self)
+
+    FileSystemModule.threadPool.submit { shouldRun in
+      defer { module.release() }
+      
+      guard case shouldRun = NIOThreadPool.WorkItemState.active else {
+        return promise.fail(ChannelError.ioOnClosedChannel)
+      }
+
+      do {
+        try writeFileSync(path, data)
+        promise.succeed( () )
+      }
+      catch {
+        promise.fail(error)
+      }
+    }
+    return promise.futureResult
+  }
+}
+
+public extension promise {
+
+  @inlinable
+  static func readdir(on eventLoop : EventLoop? = nil,
+                      _       path : String)
+              -> EventLoopFuture<[ String ]>
+  {
+    let module    = MacroCore.shared.retain()
+    let eventLoop = module.fallbackEventLoop(eventLoop)
+    let promise   = eventLoop.makePromise(of: [ String ].self)
+
+    FileSystemModule.threadPool.submit { shouldRun in
+      defer { module.release() }
+      
+      guard case shouldRun = NIOThreadPool.WorkItemState.active else {
+        return promise.fail(ChannelError.ioOnClosedChannel)
+      }
+      
+      do {
+        promise.succeed(try readdirSync(path))
+      }
+      catch {
+        promise.fail(error)
+      }
+    }
+    return promise.futureResult
+  }
+}
