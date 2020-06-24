ce9a415ebd4cfeb1d06c347cb574f1d8f17868a3
Helge Heß <devteam@zeezide.de>
2020-06-24 18:31:50 +0200

    Add process.getenvflag() helper method
    Checks for boolean environment variables.
    Allows: 1, true, enabled, YES.

--- Sources/MacroCore/Process.swift
+++ Sources/MacroCore/Process.swift
@@ -175,27 +175,57 @@ public extension process { // Warnings
 
 public extension process {
   
+  /**
+   * Checks for an integer environment variable.
+   *
+   * - Parameter environmentVariableName: The name of the environment variable.
+   * - Parameter defaultValue: Returned if the variable isn't set or can't be
+   *                           parsed.
+   * - Parameter lowerWarningBound: If the value set is below this optional
+   *                                value, a warning will be logged to the
+   *                                console.
+   * - Parameter upperWarningBound: If the value set is above this optional
+   *                                value, a warning will be logged to the
+   *                                console.
+   * - Returns: the integer value of the environment variable, or the
+   *            `defaultValue` if the environment variable wasn't set.
+   */
   @inlinable
-  static func getenv(_ environmentVarName : String,
+  static func getenv(_ environmentVariableName : String,
                      defaultValue         : Int,
                      lowerWarningBound    : Int? = nil,
                      upperWarningBound    : Int? = nil) -> Int
   {
-    if let s = process.env[environmentVarName], !s.isEmpty {
+    if let s = process.env[environmentVariableName], !s.isEmpty {
       guard let value = Int(s), value > 0 else {
-        console.error("invalid int value in env \(environmentVarName):", s)
+        console.error("invalid int value in env \(environmentVariableName):", s)
         return defaultValue
       }
       if let wv = lowerWarningBound, value < wv {
-        console.warn("pretty small \(environmentVarName) value:", value)
+        console.warn("pretty small \(environmentVariableName) value:", value)
       }
       if let wv = upperWarningBound, value > wv {
-        console.warn("pretty large \(environmentVarName) value:", value)
+        console.warn("pretty large \(environmentVariableName) value:", value)
       }
       return value
     }
     else {
       return defaultValue
     }
+  }
+  
+  /**
+   * Checks for an boolean environment variable.
+   *
+   * - Parameter environmentVariableName: The name of the environment variable.
+   * - Returns: False if the variable is not set, or none of the predefined
+   *            strings: `1`, `true`, `YES` or `enabled`.
+   */
+  @inlinable
+  static func getenvflag(_ environmentVariableName: String) -> Bool {
+    guard let s = process.env[environmentVariableName], !s.isEmpty else {
+      return false
+    }
+    return s == "1" || s == "true" || s == "YES" || s == "enabled"
   }
 }
