acc9185d570e36dc4da46e8b1346678575055678
Helge Heß <devteam@zeezide.de>
2021-01-27 17:02:20 +0100

    Add a small indexOf perf test
    Scan a 64k block 10 times.

--- Tests/MacroTests/CollectionTests.swift
+++ Tests/MacroTests/CollectionTests.swift
@@ -90,6 +90,7 @@ final class CollectionTests: XCTestCase {
     do {
       var bb = ByteBuffer()
       bb.writeBytes([ 10, 20, 30, 50, 60, 30, 40 ])
+                                       // ^^  ^^ remaining match
       
       let idxMaybe = bb.readableBytesView
             .firstIndex(of: [ 30, 40, 50, 50 ], options: .partialSuffixMatch)
@@ -120,6 +121,25 @@ final class CollectionTests: XCTestCase {
     XCTAssertEqual(idx, 0)
   }
 
+  func testByteBufferRemainingMatchPerformance() {
+    let needle : [ UInt8 ] = [ 30, 50, 60, 42, 22, 13, 37, 98, 12 ]
+
+    var bb = ByteBuffer(repeating: 0, count: 32 * 1024)
+    bb.writeBytes(needle.dropLast(2))
+    bb.writeBytes(ByteBuffer(repeating: 0, count: 32 * 1024).readableBytesView)
+    assert(bb.readableBytes > 64 * 1024)
+    
+    let start = Date()
+    measure {
+      for _ in 0..<10 {
+        let idxMaybe = bb.readableBytesView
+              .firstIndex(of: needle, options: .partialSuffixMatch)
+        XCTAssertNil(idxMaybe)
+      }
+    }
+    print("TOOK:", -start.timeIntervalSinceNow)
+  }
+
   static var allTests = [
     ( "testByteBufferSearch"              , testByteBufferSearch              ),
     ( "testByteBufferSearchNoMatch"       , testByteBufferSearchNoMatch       ),
@@ -131,6 +151,8 @@ final class CollectionTests: XCTestCase {
     ( "testByteBufferSearchRightEdge"     , testByteBufferSearchRightEdge     ),
     ( "testByteBufferRemainingMatch"      , testByteBufferRemainingMatch      ),
     ( "testByteBufferSingleItemRemainingMatch",
-      testByteBufferSingleItemRemainingMatch )
+      testByteBufferSingleItemRemainingMatch ),
+    ( "testByteBufferRemainingMatchPerformance",
+      testByteBufferRemainingMatchPerformance )
   ]
 }
