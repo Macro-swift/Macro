104fd1af301de21169d5283fc72226fbfced1183
Helge Heß <devteam@zeezide.de>
2021-04-13 18:50:47 +0200

    Add `Date.toString`
    The real one probably takes args?

--- Sources/MacroCore/JSStubs/ToString.swift
+++ Sources/MacroCore/JSStubs/ToString.swift
@@ -15,3 +15,28 @@ public extension BinaryInteger {
   @inlinable
   func toString() -> String { return toString(10) }
 }
+
+#if canImport(Foundation)
+  import struct Foundation.Date
+  import class  Foundation.DateFormatter
+
+  public extension Date {
+    
+    /* JS/Node Does:
+     * new Date().toString()
+     * 'Tue Apr 13 2021 16:24:10 GMT+0200 (Central European Summer Time)'
+     */
+    @usableFromInline
+    internal static let jsDateFmt : DateFormatter = {
+      let df = DateFormatter()
+      // Tue 13 Apr 2021 14:26:59 GMT+0000 (Coordinated Universal Time)
+      df.dateFormat = "E d MMM yyyy HH:mm:ss 'GMT'Z '('zzzz')'"
+      return df
+    }()
+    
+    @inlinable
+    func toString() -> String {
+      return Date.jsDateFmt.string(from: self)
+    }
+  }
+#endif // canImport(Foundation)
