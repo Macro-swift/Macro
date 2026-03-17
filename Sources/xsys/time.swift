51e0640e9f8328bf5c2e12acfadab268323afc1e
Helge Heß <devteam@zeezide.de>
2026-03-17 17:34:03 +0100

    Drop Swift 4.0 support
    ... and make things inlinable.

--- Sources/xsys/time.swift
+++ Sources/xsys/time.swift
@@ -3,7 +3,7 @@
 //  Noze.io / Macro
 //
 //  Created by Helge Hess on 19/05/16.
-//  Copyright © 2016-2020 ZeeZide GmbH. All rights reserved.
+//  Copyright © 2016-2026 ZeeZide GmbH. All rights reserved.
 //
 
 #if os(Windows)
@@ -41,17 +41,20 @@
 public extension time_t {
   
   /// Returns the current time.
+  @inlinable    
   static var now : time_t { return xsys.time(nil) }
   
   /// Initialize the `time_t` value from Unix `tm` value (date components).
   /// Assumes the values are given in *local time*.
   /// Remember that the `time_t` itself is in UTC.
+  @inlinable    
   init(_ tm: xsys.struct_tm) {
     self = tm.localTime
   }
   /// Initialize the `time_t` value from Unix `tm` value (date components).
   /// Assumes the values are given in *UTC time*.
   /// Remember that the `time_t` itself is in UTC.
+  @inlinable    
   init(utc tm: xsys.struct_tm) {
     self = tm.utcTime
   }
@@ -59,6 +62,7 @@ public extension time_t {
   /// Converts the `time_t` timestamp into date components (`tz` struct) living
   /// in the UTC timezone.
   /// Remember that the `time_t` itself is in UTC.
+  @inlinable    
   var componentsInUTC : xsys.struct_tm {
     var t  = self
     var tm = xsys.struct_tm()
@@ -69,6 +73,7 @@ public extension time_t {
   /// Converts the `time_t` timestamp into date components (`tz` struct) living
   /// in the local timezone of the Unix environment.
   /// Remember that the `time_t` itself is in UTC.
+  @inlinable    
   var componentsInLocalTime : xsys.struct_tm {
     var t  = self
     var tm = xsys.struct_tm()
@@ -86,6 +91,7 @@ public extension time_t {
   ///
   ///     xsys.time(nil).format("%a, %d %b %Y %H:%M:%S %Z")
   ///
+  @inlinable    
   func format(_ sf: String) -> String {
     return self.componentsInUTC.format(sf)
   }
@@ -97,20 +103,24 @@ public extension xsys.struct_tm {
   
   /// Create a Unix date components structure from a timestamp. This variant
   /// creates components in the local timezone.
+  @inlinable    
   init(_ tm: time_t) {
     self = tm.componentsInLocalTime
   }
   
   /// Create a Unix date components structure from a timestamp. This variant
   /// creates components in the UTC timezone.
+  @inlinable    
   init(utc tm: time_t) {
     self = tm.componentsInUTC
   }
   
+  @inlinable    
   var utcTime : time_t {
     var tm = self
     return timegm(&tm)
   }
+  @inlinable    
   var localTime : time_t {
     var tm = self
     return mktime(&tm)
@@ -119,6 +129,7 @@ public extension xsys.struct_tm {
   /// Example `strftime` format (`man strftime`):
   ///   "%a, %d %b %Y %H:%M:%S GMT"
   ///
+  @inlinable    
   func format(_ sf: String, defaultCapacity: Int = 100) -> String {
     var tm = self
     
@@ -128,20 +139,12 @@ public extension xsys.struct_tm {
     var capacity = attempt1Capacity
     
     var buf = UnsafeMutablePointer<CChar>.allocate(capacity: capacity)
-    #if swift(>=4.1)
-      defer { buf.deallocate() }
-    #else
-      defer { buf.deallocate(capacity: capacity) }
-    #endif
+    defer { buf.deallocate() }
   
     let rc = xsys.strftime(buf, capacity, sf, &tm)
   
     if rc == 0 {
-      #if swift(>=4.1)
-        buf.deallocate()
-      #else
-        buf.deallocate(capacity: capacity)
-      #endif
+      buf.deallocate()
       capacity = attempt2Capacity
       buf = UnsafeMutablePointer<CChar>.allocate(capacity: capacity)
   
