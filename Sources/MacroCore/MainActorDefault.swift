773beb151095ff9fb4a5bd314bbf14915f4c9078
Helge Heß <devteam@zeezide.de>
2026-03-19 14:27:36 +0100

    Restrict MainActor overload to Swift 6.0
    There it should be part of the type.

--- Sources/MacroCore/MainActorDefault.swift
+++ Sources/MacroCore/MainActorDefault.swift
@@ -6,48 +6,45 @@
 //  Copyright (C) 2026 ZeeZide GmbH. All rights reserved.
 //
 
-#if swift(>=5.5) && canImport(_Concurrency)
+#if swift(>=6.0)
 import NIO
 
-@available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
 public extension MacroCore {
   // HH: Preserve MainActor isolation if enqueued there.
 
   @inlinable
   func nextTick(on eventLoop: EventLoop? = nil,
-                mainActor: @escaping @MainActor () -> Void)
+                _ execute: @escaping @MainActor () -> Void)
   {
     retain()
     nextTick(on: eventLoop, {
-      Task { @MainActor in mainActor(); self.release() }
+      Task { @MainActor in execute(); self.release() }
     })
   }
 
   @inlinable
-  func setTimeout(on eventLoop: EventLoop? = nil, _  milliseconds: Int,
-                  mainActor: @escaping @MainActor () -> Void)
+  func setTimeout(on eventLoop: EventLoop? = nil, _ milliseconds: Int,
+                  _ execute: @escaping @MainActor () -> Void)
   { // TBD: This could just schedule on the mainActor if EL=nil?
     retain()
     setTimeout(on: eventLoop, milliseconds, {
-      Task { @MainActor in mainActor(); self.release() }      
+      Task { @MainActor in execute(); self.release() }
     })
   }
 }
 
 @inlinable
-@available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
 public func nextTick(on eventLoop : EventLoop? = nil,
-                     mainActor execute: @escaping @MainActor () -> Void)
+                     _ execute: @escaping @MainActor () -> Void)
 {
-  MacroCore.shared.nextTick(on: eventLoop, mainActor: execute)
+  MacroCore.shared.nextTick(on: eventLoop, execute)
 }
 
 @inlinable
-@available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
 public func setTimeout(on eventLoop : EventLoop? = nil, _ milliseconds: Int,
-                       mainActor execute:@escaping @MainActor () -> Void)
+                       _ execute: @escaping @MainActor () -> Void)
 {
-  MacroCore.shared.setTimeout(on: eventLoop, milliseconds, mainActor: execute)
+  MacroCore.shared.setTimeout(on: eventLoop, milliseconds, execute)
 }
 
 #endif
