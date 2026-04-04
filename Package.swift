3bc8c861cd7aa4783a8c4c8bad30f7baa957a945
Helge Heß <devteam@zeezide.de>
2026-04-04 17:13:41 +0200

    Drop Swift 5.9 support
    We need Span's, and Span-NIO seems to require 6.0.

--- Package.swift
+++ Package.swift
@@ -1,13 +1,16 @@
-// swift-tools-version:5.5
+// swift-tools-version:6.0
 
 import PackageDescription
 
 let package = Package(
-  
+
   name: "Macro",
-  
+
+  platforms: [ .macOS(.v10_15), .iOS(.v13) ],
+
   products: [
     .library(name: "Macro",              targets: [ "Macro"              ]),
+    .library(name: "Macro6",             targets: [ "Macro6"             ]),
     .library(name: "MacroCore",          targets: [ "MacroCore"          ]),
     .library(name: "xsys",               targets: [ "xsys"               ]),
     .library(name: "http",               targets: [ "http"               ]),
@@ -15,16 +18,13 @@ let package = Package(
     .library(name: "ws",                 targets: [ "ws"                 ]),
     .library(name: "MacroTestUtilities", targets: [ "MacroTestUtilities" ])
   ],
-  
+
   dependencies: [
-    .package(url: "https://github.com/apple/swift-atomics.git",
-             from: "1.0.3"),
-    .package(url: "https://github.com/apple/swift-nio.git",
-             from: "2.97.0"),
-    .package(url: "https://github.com/apple/swift-log.git",
-             from: "1.4.4")
+    .package(url: "https://github.com/apple/swift-atomics.git", from: "1.0.3"),
+    .package(url: "https://github.com/apple/swift-nio.git",     from: "2.80.0"),
+    .package(url: "https://github.com/apple/swift-log.git",     from: "1.4.4")
   ],
-  
+
   targets: [
     .target(name: "MacroCore",
             dependencies: [
@@ -34,22 +34,33 @@ let package = Package(
               .product(name: "NIOFoundationCompat",   package: "swift-nio"),
               .product(name: "Logging",               package: "swift-log"),
               "xsys"
-            ], exclude: [ "Process/README.md", "Streams/README.md" ]),
-    .target(name: "xsys", exclude: [ "README.md" ]),
+            ],
+            exclude: [
+              "Process/README.md", "Streams/README.md"
+            ],
+            swiftSettings: [
+              .swiftLanguageMode(.v5),
+              .enableExperimentalFeature("Lifetimes")
+            ]),
+    .target(name: "xsys",
+            exclude: [ "README.md" ],
+            swiftSettings: [ .swiftLanguageMode(.v5) ]),
     .target(name: "http",
-            dependencies: [ 
+            dependencies: [
               .product(name: "NIO",                   package: "swift-nio"),
               .product(name: "NIOConcurrencyHelpers", package: "swift-nio"),
               .product(name: "NIOHTTP1",              package: "swift-nio"),
               "MacroCore"
             ],
-            exclude: [ "README.md" ]),
+            exclude: [ "README.md" ],
+            swiftSettings: [ .swiftLanguageMode(.v5) ]),
     .target(name: "fs",
             dependencies: [
               .product(name: "NIO", package: "swift-nio"),
               "MacroCore", "xsys"
             ],
-            exclude: [ "README.md" ]),
+            exclude: [ "README.md" ],
+            swiftSettings: [ .swiftLanguageMode(.v5) ]),
     .target(name: "ws",
             dependencies: [
               .product(name: "NIO",                   package: "swift-nio"),
@@ -57,17 +68,35 @@ let package = Package(
               .product(name: "NIOWebSocket",          package: "swift-nio"),
               "MacroCore", "http"
             ],
-            exclude: [ "README.md" ]),
-    
+            exclude: [ "README.md" ],
+            swiftSettings: [ .swiftLanguageMode(.v5) ]),
+
+    .target(name: "Macro6",
+            dependencies: [
+              "MacroCore",
+              .product(name: "NIO", package: "swift-nio")
+            ],
+            swiftSettings: [ .swiftLanguageMode(.v6) ]),
+
     // This is the Umbrella Target
-    .target(name: "Macro", dependencies: [ "MacroCore", "xsys", "http", "fs" ],
-            exclude: [ "README.md" ]),
-    
-    
+    .target(name: "Macro",
+            dependencies: [ "MacroCore", "xsys", "http", "fs", "Macro6" ],
+            exclude: [ "README.md" ],
+            swiftSettings: [ .swiftLanguageMode(.v6) ]),
+
+
     // MARK: - Tests
 
-    .target(name: "MacroTestUtilities", dependencies: [ "Macro" ]),
+    .target(name: "MacroTestUtilities",
+            dependencies: [ "Macro" ],
+            swiftSettings: [ .swiftLanguageMode(.v5) ]),
 
-    .testTarget(name: "MacroTests", dependencies: [ "MacroTestUtilities", "ws" ])
+    .testTarget(name: "Macro6Tests",
+                dependencies: [ "Macro6", "MacroTestUtilities" ],
+                swiftSettings: [ .swiftLanguageMode(.v6) ]),
+
+    .testTarget(name: "MacroTests",
+                dependencies: [ "MacroTestUtilities", "ws" ],
+                swiftSettings: [ .swiftLanguageMode(.v5) ])
   ]
 )
